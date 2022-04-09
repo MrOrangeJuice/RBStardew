6a5d1f4934cc05c4a34c1308deb7925994d14b31
Nick-Malone <>
2022-04-09 17:08:39 -0400

    Revamp main island

--- Map/BeachMapTileset.tsx
+++ Map/BeachMapTileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.2" name="untitled tile sheet2" tilewidth="16" tileheight="16" tilecount="1280" columns="32">
- <image source="../../../../Program Files (x86)/Steam/steamapps/common/Stardew Valley/Content (unpacked)/Maps/spring_island_tilesheet_1" width="512" height="640"/>
+ <image source="../Content (unpacked)/Maps/spring_island_tilesheet_1.png" width="512" height="640"/>
  <tile id="7">
   <properties>
    <property name="Water" value="T"/>
