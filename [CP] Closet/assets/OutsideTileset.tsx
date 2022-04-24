8e1de8c0c04a616d6c4b27027b2c1975428961ba
Nick-Malone <>
2022-04-24 19:00:35 -0400

    update map stuff in closet folder

--- [CP] Closet/assets/OutsideTileset.tsx
+++ [CP] Closet/assets/OutsideTileset.tsx
@@ -1,9 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="spring_outdoorsTileSheet" tilewidth="16" tileheight="16" tilecount="1975" columns="25">
- <image source="spring_outdoorsTileSheet.png" width="400" height="1264"/>
- <tile id="0">
-  <properties>
-   <property name="name" value="spring_outdoorsTileSheet"/>
-  </properties>
- </tile>
+<tileset version="1.8" tiledversion="1.8.2" name="OutsideTileset" tilewidth="16" tileheight="16" tilecount="1975" columns="25">
+ <image source="spring_outdoorsTileSheet.es-ES.png" width="400" height="1264"/>
 </tileset>
