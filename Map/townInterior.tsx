e33932b69f6d41b3dcb00c8aa0f646172044cfc8
oracra <nfa8750@rit.edu>
2022-04-24 16:55:44 -0400

    Updated Maps to include all
    included

--- Map/townInterior.tsx
+++ Map/townInterior.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.2" name="townInterior" tilewidth="16" tileheight="16" tilecount="2176" columns="32">
- <image source="../Content (unpacked)/Maps/townInterior.png" width="512" height="1088"/>
+ <image source="townInterior.png" width="512" height="1088"/>
 </tileset>
