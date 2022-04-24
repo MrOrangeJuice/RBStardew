436b8987b4e518e101385f47cb02f74fe6b498e2
Nick-Malone <>
2022-04-24 16:54:29 -0400

    Map changes

---
+++ Map/spring_beach.es-ES.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="spring_beach.es-ES" tilewidth="16" tileheight="16" tilecount="527" columns="17">
+ <image source="../Content (unpacked)/Maps/spring_beach.es-ES.png" width="272" height="496"/>
+</tileset>
