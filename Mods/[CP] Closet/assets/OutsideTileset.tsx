f72c06cb46bba59f544b038af9e1760ce5fb344d
oracra <nfa8750@rit.edu>
2022-04-24 20:37:06 -0400

    updated mods

---
+++ Mods/[CP] Closet/assets/OutsideTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="OutsideTileset" tilewidth="16" tileheight="16" tilecount="1975" columns="25">
+ <image source="spring_outdoorsTileSheet.es-ES.png" width="400" height="1264"/>
+</tileset>
---
+++ Mods/[CP] Closet/assets/witchHut.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="witchHut" tilewidth="16" tileheight="16" tilecount="128" columns="8">
+    <image source="WitchHutTiles.png" width="128" height="256" />
+ <tile id="32">
+  <animation>
+   <frame tileid="32" duration="1000"/>
+   <frame tileid="32" duration="1000"/>
+   <frame tileid="32" duration="1000"/>
+   <frame tileid="32" duration="1000"/>
+   <frame tileid="32" duration="1000"/>
+   <frame tileid="35" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="40">
+  <animation>
+   <frame tileid="40" duration="101"/>
+   <frame tileid="41" duration="101"/>
+   <frame tileid="40" duration="101"/>
+   <frame tileid="42" duration="101"/>
+   <frame tileid="43" duration="101"/>
+   <frame tileid="40" duration="101"/>
+   <frame tileid="43" duration="101"/>
+   <frame tileid="40" duration="101"/>
+   <frame tileid="43" duration="101"/>
+   <frame tileid="41" duration="101"/>
+   <frame tileid="43" duration="101"/>
+   <frame tileid="42" duration="101"/>
+   <frame tileid="43" duration="101"/>
+  </animation>
+ </tile>
+</tileset>
